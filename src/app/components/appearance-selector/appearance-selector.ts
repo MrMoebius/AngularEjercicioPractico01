13e68e98909f92b64bf654f04730cba6bbefaf8c
MrMoebius <e.spadaro.ro@gmail.com>
2026-01-13 19:34:31 +0100

    appearance-selector

--- src/app/components/appearance-selector/appearance-selector.ts
+++ src/app/components/appearance-selector/appearance-selector.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CharacterBuild } from '../../services/character-build';
 
 @Component({
   selector: 'app-appearance-selector',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './appearance-selector.html',
   styleUrl: './appearance-selector.css',
 })
 export class AppearanceSelector {
+  private characterService = inject(CharacterBuild);
+  
+  descripcion: string = '';
+  imagen: string = '';
 
+  onDescripcionChange() {
+    this.characterService.updateDescripcion(this.descripcion);
+  }
+
+  onImagenChange() {
+    this.characterService.updateImagen(this.imagen);
+  }
 }
