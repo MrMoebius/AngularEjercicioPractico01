a1c12f956bde21ee564e4f063ad9b36bd2dbb91b
MrMoebius <e.spadaro.ro@gmail.com>
2026-01-13 19:45:50 +0100

    Montar pagina con todos componentes usando el service.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { IdentitySelector } from './components/identity-selector/identity-selector';
+import { ClasEquipSelector } from './components/class-equip-selector/class-equip-selector';
+import { AppearanceSelector } from './components/appearance-selector/appearance-selector';
+import { CharacterSummary } from './components/character-summary/character-summary';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    IdentitySelector,
+    ClasEquipSelector,
+    AppearanceSelector,
+    CharacterSummary
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularEjercicioPractico01');
 }
